450ab35a4a586d0004512f73f8832db937b7d5fd
Delfat <fatshinli@gmail.com>
2025-11-24 14:21:15 -0500

    Task 2.4

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -30,26 +30,42 @@ public class StatementPrinter {
      * @throws RuntimeException if one of the play types is not known
      */
     public String statement() {
-        int totalAmount = 0;
-        int volumeCredits = 0;
+
         final StringBuilder result = new StringBuilder("Statement for " + invoice.getCustomer()
                 + System.lineSeparator());
 
+        int rslt = 0;
         for (Performance performance : invoice.getPerformances()) {
 
-            int rslt = 0;
             rslt = getAmount(performance);
-            
-            // add volume credits
-            volumeCredits += getVolumeCredits(performance);
+
             // print line for this order
             result.append(String.format("  %s: %s (%s seats)%n", getPlay(performance).getName(),
                     usd(rslt), performance.getAudience()));
-            totalAmount += rslt;
         }
-        result.append(String.format("Amount owed is %s%n", usd(totalAmount)));
-        result.append(String.format("You earned %s credits%n", volumeCredits));
+        result.append(String.format("Amount owed is %s%n", usd(getTotalAmount())));
+        result.append(String.format("You earned %s credits%n", getTotalVolumeCredits()));
         return result.toString();
+    }
+
+    private int getTotalAmount() {
+        int result = 0;
+        for (Performance performance : invoice.getPerformances()) {
+            int rslt = 0;
+            rslt = getAmount(performance);
+            result += rslt;
+        }
+        return result;
+    }
+
+    private int getTotalVolumeCredits() {
+        int result = 0;
+        for (Performance performance : invoice.getPerformances()) {
+
+            // add volume credits
+            result += getVolumeCredits(performance);
+        }
+        return result;
     }
 
     private static String usd(int money) {
